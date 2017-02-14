40e679046f13efb073bed3b016cb1be9d7179412
Darius I. Karel <me@dikarel.com>
2017-02-13 20:15:18 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,62 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es6: true,
+    jest: true,
+    mocha: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  globals: {
+    ga: false,
+    google: false,
+    fbq: false,
+    Stripe: false,
+    console: false,
+    fetch: false,
+    navigator: false,
+    FormData: false,
+    __DEV__: false,
+    __dirname: false,
+    __filename: false,
+    driver: false,
+    process: false,
+    assert: false,
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 7,
+    sourceType: 'module',
+    ecmaFeatures: {
+      modules: true,
+      experimentalObjectRestSpread: true,
+      jsx: true,
+    },
+  },
+  plugins: [
+    'react',
+    'react-native',
+  ],
+  rules: {
+    // Allow importing modules without specifying extension
+    'import/extensions': ['error', 'never'],
+
+    'import/no-extraneous-dependencies': 0,
+    'import/no-unresolved': 0,
+    'linebreak-style': ['error', 'unix'],
+    'react/jsx-filename-extension': 0,
+    'react/jsx-uses-react': 'error',
+    'react/jsx-uses-vars': 'error',
+    'react/jsx-no-bind': [2, {
+      "ignoreRefs": false,
+      "allowArrowFunctions": false,
+      "allowBind": false,
+    }],
+    indent: ['error', 2],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+  }
+};
---
+++ readme.md
@@ -0,0 +1,13 @@
+# eslint-config-instawork
+
+[ESLint](http://eslint.org) configuration for [Instawork projects](http://github.com/instawork)
+
+## Quickstart
+
+**.eslintrc.js**
+
+```js
+{
+  extends: ['instawork']
+}
+```
