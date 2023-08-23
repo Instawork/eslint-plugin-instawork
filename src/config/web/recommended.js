0de4b5a0fc329d8473bddd660c4613e3831208ee
Sameer Kumar <sameerk1292@gmail.com>
2023-08-23 15:57:15 +0530

    Added no-console rule in web recommended config

--- src/config/web/recommended.js
+++ src/config/web/recommended.js
@@ -23,6 +23,19 @@ const overrides = [
   {
     ...webTypescriptConfig,
   },
+  // ignore console statements in storybook file and test files
+  {
+    files: [
+      '*.stories.[jt]s?(x)',
+      'stories.[jt]s?(x)',
+      '*.test.[jt]s?(x)',
+      // all files under any folder named test
+      '**/test/**/*.[jt]s?(x)',
+    ],
+    rules: {
+      'no-console': 'off',
+    },
+  },
 ];
 
 module.exports = {
